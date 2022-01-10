09e939e49c5bd1779778dacb6423e45c56672ec0
kayua <kayuaolequesp@gmail.com>
2022-01-10 20:20:33 -0300

    update

--- tools/dataset/dataset.py
+++ tools/dataset/dataset.py
@@ -7,7 +7,7 @@ class Dataset:
 
         self.snapshot_column_position = 1
         self.peer_column_position = 2
-        self.feature_window_length = 12
+        self.feature_window_length = 4
         self.feature_window_width = 32
         self.break_point = 1
         self.matrix_features = []
@@ -21,15 +21,15 @@ class Dataset:
 
         if self.feature_window_length % 2:
             print('Erro: Use um numero de base binaria para de comprimento')
-            exit(-1)
+            #exit(-1)
 
         if self.number_block_per_samples % 2:
             print('Erro: Use um numero de base binaria para de blocos')
-            exit(-1)
+            #exit(-1)
 
         if self.feature_window_width % 2:
             print('Erro: Use um numero de base binaria para de largura')
-            exit(-1)
+            #exit(-1)
 
         size_matrix_allocation_width = self.feature_window_width * self.number_block_per_samples
 
@@ -47,7 +47,8 @@ class Dataset:
 
     def insert_in_matrix(self, snapshot_id, peer_id):
 
-        self.matrix_features[peer_id][(snapshot_id % self.feature_window_length)-1] = 1
+        print('{} {} '.format(peer_id, (snapshot_id % self.feature_window_length)))
+        self.matrix_features[peer_id][(snapshot_id % self.feature_window_length)] = 1
 
     def create_samples(self):
 
@@ -55,48 +56,74 @@ class Dataset:
 
         for i in range(0, self.feature_window_width*self.number_block_per_samples, self.feature_window_width):
 
-            results.append(self.matrix_features[i:i+self.feature_window_width])
+            results.append(numpy.array(self.matrix_features[i:i+self.feature_window_width]))
 
-
-
-
-
-
-
-
+        self.feature_input.extend(numpy.array(results))
 
     def load_swarm_to_feature(self):
 
         self.allocation_matrix()
         file_pointer_swarm = open(self.input_file_swarm_sorted, 'r')
         line_swarm_file = file_pointer_swarm.readlines()
-        last_snapshot_read = 0
+        break_point = 0
 
-        for _, swarm_line in enumerate(line_swarm_file):
+
+        for i, swarm_line in enumerate(line_swarm_file):
 
             swarm_line_in_list = swarm_line.split(' ')
             snapshot_value = int(swarm_line_in_list[self.snapshot_column_position - 1])
             peer_value = int(swarm_line_in_list[self.peer_column_position - 1])
+            if i == 0:
+                break_point = snapshot_value
 
-            if (snapshot_value % self.feature_window_length == 0) and last_snapshot_read != 0:
 
-                last_snapshot_read = snapshot_value
+            print('Verificar como resolver o problema do zero no Hash')
+            exit(-1)
+            if (snapshot_value % self.feature_window_length+1 == (self.feature_window_length)) and break_point !=snapshot_value:
+
                 self.insert_in_matrix(snapshot_value, peer_value)
                 self.create_samples()
                 self.clean_matrix()
+                break_point = snapshot_value
 
             else:
 
                 self.insert_in_matrix(snapshot_value, peer_value)
 
-        self.create_samples()
-        self.clean_matrix()
+    @staticmethod
+    def concatenate(list_matrix):
+
+        results = []
+
+        for i in list_matrix:
+
+            results.extend(i)
+
+        return numpy.array(results)
+
+    def cast_feature_to_swarm(self):
+        result = []
+        for i in range(0, len(self.feature_input), self.number_block_per_samples):
+            result.append(self.concatenate(self.feature_input[i:i+self.number_block_per_samples]))
+
+        x = numpy.array(result)
+
+        for i in range(len(x)):
+
+            for j in range(len(x[0])):
+
+                print(x[i][j])
+
+            print('\n')
 
     def show_matrix(self):
 
-        for i in range(len(self.matrix_features)):
 
-            print(self.matrix_features[i])
+        for i in range(len(self.feature_input)):
+
+            for j in range(len(self.feature_input[0])):
+
+                print(self.feature_input[i][j])
 
             print('\n')
 
@@ -107,4 +134,4 @@ class Dataset:
 
 a = Dataset()
 a.load_swarm_to_feature()
-a.show_matrix()
+a.cast_feature_to_swarm()
