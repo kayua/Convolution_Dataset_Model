ddfea43c5c44eee65796f4dff0863faa28819c00
kayua <kayuaolequesp@gmail.com>
2022-01-13 14:56:16 -0300

    update

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 INPUT_DATASET_TRAINING_IN='dataset/training/failed_training/S4'
-INPUT_DATASET_TRAINING_OUT='dataset/training/original_training/S4'
 OUTPUT_DATASET_TRAINING_IN='samples_saved/samples_training_in/S4'
+
+INPUT_DATASET_TRAINING_OUT='dataset/training/original_training/S4'
 OUTPUT_DATASET_TRAINING_OUT='samples_saved/samples_training_out/S4'
 
 INPUT_DATASET_PREDICT_IN='dataset/predict/failed_training/S4'
@@ -8,7 +9,6 @@ OUTPUT_DATASET_PREDICT_OUT='samples_saved/samples_predict/S4'
 
 
 
-
-
-
-python3 main.py CreateSamples --input_file_in    --output_file_in
+python3 main.py CreateSamples --input_file_in $INPUT_DATASET_TRAINING_IN --output_file_in $OUTPUT_DATASET_TRAINING_IN
+python3 main.py CreateSamples --input_file_in $INPUT_DATASET_TRAINING_OUT --output_file_in $OUTPUT_DATASET_TRAINING_OUT
+python3 main.py CreateSamples --input_file_in $INPUT_DATASET_PREDICT_IN --output_file_in $OUTPUT_DATASET_PREDICT_OUT
