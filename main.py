d9bc87dac5de770c0a406ecaa3567065745c9e05
kayua <kayuaolequesp@gmail.com>
2022-01-13 06:55:08 -0300

    update

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from models.neural import Neural
 from models.neural_models.model_v1 import ModelsV1
+from tools.dataset.dataset import Dataset
 
 
 def calibration_neural_model(args):
-
     neural_network_instance = create_classifier_model(args)
     neural_network_instance.calibration_neural_network()
 
 
-def create_classifier_model(args):
+def create_samples(args):
+    dataset_instance = Dataset(args)
+    dataset_instance.load_file_samples()
+    dataset_instance.save_file_samples()
 
+
+def create_classifier_model(args):
     neural_model = Neural(args)
 
     if args.file_load_model != '':
@@ -22,7 +27,6 @@ def create_classifier_model(args):
 
 
 def arguments_cmd_choice(args):
-
     if args.cmd == 'Calibration': calibration_neural_model(args)
     if args.cmd == 'CreateSamples': create_samples(args)
     if args.cmd == 'Training': training_neural_model(args)
