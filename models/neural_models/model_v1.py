958b1ca985a5931dcb7b269e3b3f7ad646242433
kayua <kayuaolequesp@gmail.com>
2022-01-15 15:10:56 -0300

    update

--- models/neural_models/model_v1.py
+++ models/neural_models/model_v1.py
@@ -7,16 +7,17 @@ __version__ = '{2}.{0}.{1}'
 __data__ = '2021/11/21'
 __credits__ = ['All']
 
-import numpy
 from tensorflow.keras import Input, activations, Model
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import ZeroPadding2D
 from tensorflow.keras.layers import Add
+from tensorflow.keras.layers import Flatten, Reshape, Dense
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import UpSampling2D
 from tensorflow.keras.layers import BatchNormalization
 from models.neural_models.neural_model import NeuralModel
+import numpy
 
 
 class ModelsV1(NeuralModel):
@@ -29,43 +30,50 @@ class ModelsV1(NeuralModel):
     def create_neural_network(self):
 
         input_layer_block = Input(shape=(self.feature_window_width, self.feature_window_length, 1))
-        first_convolution_block = Conv2D(16, (3, 3))(input_layer_block)
+        first_convolution_block = Conv2D(128, (3, 3))(input_layer_block)
         first_convolution_block = Activation(activations.relu)(first_convolution_block)
         first_convolution_block = ZeroPadding2D((1, 1))(first_convolution_block)
         first_convolution_block = MaxPooling2D((2, 2))(first_convolution_block)
         first_convolution_block = BatchNormalization()(first_convolution_block)
 
-        second_convolution_block = Conv2D(16, (3, 3))(first_convolution_block)
+        second_convolution_block = Conv2D(128, (3, 3))(first_convolution_block)
         second_convolution_block = Activation(activations.relu)(second_convolution_block)
         second_convolution_block = ZeroPadding2D((1, 1))(second_convolution_block)
         second_convolution_block = MaxPooling2D((2, 2))(second_convolution_block)
         second_convolution_block = BatchNormalization()(second_convolution_block)
 
-        third_convolution_block = Conv2D(16, (3, 3))(second_convolution_block)
+        third_convolution_block = Conv2D(128, (3, 3))(second_convolution_block)
         third_convolution_block = Activation(activations.relu)(third_convolution_block)
         third_convolution_block = ZeroPadding2D((1, 1))(third_convolution_block)
         third_convolution_block = MaxPooling2D((2, 2))(third_convolution_block)
         third_convolution_block = BatchNormalization()(third_convolution_block)
 
-        fourth_convolution_block = Conv2D(16, (3, 3))(third_convolution_block)
+        fourth_convolution_block = Conv2D(128, (3, 3))(third_convolution_block)
         fourth_convolution_block = Activation(activations.relu)(fourth_convolution_block)
         fourth_convolution_block = ZeroPadding2D((1, 1))(fourth_convolution_block)
         fourth_convolution_block = MaxPooling2D((2, 2))(fourth_convolution_block)
         fourth_convolution_block = BatchNormalization()(fourth_convolution_block)
 
-        fifth_convolution_block = Conv2D(16, (3, 3))(fourth_convolution_block)
+        fifth_convolution_block = Conv2D(128, (3, 3))(fourth_convolution_block)
         fifth_convolution_block = Activation(activations.relu)(fifth_convolution_block)
         fifth_convolution_block = ZeroPadding2D((1, 1))(fifth_convolution_block)
         fifth_convolution_block = MaxPooling2D((2, 2))(fifth_convolution_block)
         fifth_convolution_block = BatchNormalization()(fifth_convolution_block)
 
-        sixth_convolution_block = Conv2D(16, (3, 3))(fifth_convolution_block)
+        sixth_convolution_block = Conv2D(128, (3, 3))(fifth_convolution_block)
         sixth_convolution_block = Activation(activations.relu)(sixth_convolution_block)
         sixth_convolution_block = ZeroPadding2D((1, 1))(sixth_convolution_block)
         sixth_convolution_block = MaxPooling2D((2, 2))(sixth_convolution_block)
         sixth_convolution_block = BatchNormalization()(sixth_convolution_block)
 
-        firts_upsampling_block = Conv2D(16, (3, 3))(sixth_convolution_block)
+        bottleneck = Flatten()(sixth_convolution_block)
+        bottleneck = Dense(512)(bottleneck)
+        bottleneck = Activation(activations.relu)(bottleneck)
+        bottleneck = Dense(512)(bottleneck)
+        bottleneck = Activation(activations.relu)(bottleneck)
+        bottleneck = Reshape((4, 4, 32))(bottleneck)
+
+        firts_upsampling_block = Conv2D(128, (3, 3))(bottleneck)
         firts_upsampling_block = Activation(activations.relu)(firts_upsampling_block)
         firts_upsampling_block = ZeroPadding2D((1, 1))(firts_upsampling_block)
         firts_upsampling_block = UpSampling2D((2, 2))(firts_upsampling_block)
@@ -73,7 +81,7 @@ class ModelsV1(NeuralModel):
 
         interpolation = Add()([fifth_convolution_block, firts_upsampling_block])
 
-        second_upsampling_block = Conv2D(16, (3, 3))(interpolation)
+        second_upsampling_block = Conv2D(128, (3, 3))(interpolation)
         second_upsampling_block = Activation(activations.relu)(second_upsampling_block)
         second_upsampling_block = ZeroPadding2D((1, 1))(second_upsampling_block)
         second_upsampling_block = UpSampling2D((2, 2))(second_upsampling_block)
@@ -81,7 +89,7 @@ class ModelsV1(NeuralModel):
 
         interpolation = Add()([fourth_convolution_block, second_upsampling_block])
 
-        third_upsampling_block = Conv2D(16, (3, 3))(interpolation)
+        third_upsampling_block = Conv2D(128, (3, 3))(interpolation)
         third_upsampling_block = Activation(activations.relu)(third_upsampling_block)
         third_upsampling_block = ZeroPadding2D((1, 1))(third_upsampling_block)
         third_upsampling_block = UpSampling2D((2, 2))(third_upsampling_block)
@@ -89,7 +97,7 @@ class ModelsV1(NeuralModel):
 
         interpolation = Add()([third_convolution_block, third_upsampling_block])
 
-        fourth_upsampling_block = Conv2D(16, (3, 3))(interpolation)
+        fourth_upsampling_block = Conv2D(128, (3, 3))(interpolation)
         fourth_upsampling_block = Activation(activations.relu)(fourth_upsampling_block)
         fourth_upsampling_block = ZeroPadding2D((1, 1))(fourth_upsampling_block)
         fourth_upsampling_block = UpSampling2D((2, 2))(fourth_upsampling_block)
@@ -97,7 +105,7 @@ class ModelsV1(NeuralModel):
 
         interpolation = Add()([second_convolution_block, fourth_upsampling_block])
 
-        fifth_upsampling_block = Conv2D(16, (3, 3))(interpolation)
+        fifth_upsampling_block = Conv2D(128, (3, 3))(interpolation)
         fifth_upsampling_block = Activation(activations.relu)(fifth_upsampling_block)
         fifth_upsampling_block = ZeroPadding2D((1, 1))(fifth_upsampling_block)
         fifth_upsampling_block = UpSampling2D((2, 2))(fifth_upsampling_block)
@@ -105,7 +113,7 @@ class ModelsV1(NeuralModel):
 
         interpolation = Add()([first_convolution_block, fifth_upsampling_block])
 
-        sixth_convolution_block = Conv2D(16, (3, 3))(interpolation)
+        sixth_convolution_block = Conv2D(128, (3, 3))(interpolation)
         sixth_convolution_block = Activation(activations.relu)(sixth_convolution_block)
         sixth_convolution_block = ZeroPadding2D((1, 1))(sixth_convolution_block)
         sixth_convolution_block = UpSampling2D((2, 2))(sixth_convolution_block)
@@ -150,12 +158,18 @@ class ModelsV1(NeuralModel):
         return numpy.array(x_training_list), numpy.array(y_training_list)
 
     def training(self, x_training, y_training, evaluation_set):
-
+        print(x_training.shape)
+        x_training, y_training = self.remove_empty_features(x_training, y_training)
+        print(x_training.shape)
         for i in range(self.epochs):
 
             random_array_feature = self.get_random_batch(x_training)
             samples_batch_training_in = self.get_feature_batch(x_training, random_array_feature)
             samples_batch_training_out = self.get_feature_batch(y_training, random_array_feature)
-            self.model.fit(x=samples_batch_training_in, y=samples_batch_training_out, verbose=2)
+            self.model.fit(x=samples_batch_training_in, y=samples_batch_training_out, epochs=1, verbose=1)
+
+            if i % 10 == 0:
+                b = self.model.predict(samples_batch_training_in[0:100])
+                self.save_image_feature(b[0:100], samples_batch_training_out[0:100], i)
 
         return 0
