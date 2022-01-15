986374842648bf49fc6393a1922e8472d2871da2
kayua <kayuaolequesp@gmail.com>
2022-01-15 15:01:22 -0300

    update

--- models/neural.py
+++ models/neural.py
@@ -49,19 +49,21 @@ class Neural:
             self.neural_network = NeuralModel(self.args)
             logging.info('Loading neural network model')
 
-            neural_model_json = open('{}.json'.format(self.file_load_model), 'r')
-            logging.debug('Loaded file {}.json'.format(self.file_load_model))
+            neural_model_json = open('{}.json'.format('models_saved/model'), 'r')
+            logging.debug('Loaded file {}.json'.format('models_saved/model'))
             if self.adversarial_model:
 
                 self.neural_network.generator_model = model_from_json(neural_model_json.read())
-                self.neural_network.generator_model.load_weights('{}.h5'.format(self.file_load_model))
-                self.neural_network.generator_model.compile(loss=self.loss, optimizer=self.optimizer, metrics=self.metrics)
+                self.neural_network.generator_model.load_weights('{}.h5'.format('models_saved/model'))
+                self.neural_network.generator_model.compile(loss=self.loss, optimizer=self.optimizer,
+                                                            metrics=self.metrics)
             else:
 
                 self.neural_network.model = model_from_json(neural_model_json.read())
-                self.neural_network.model.load_weights('{}.h5'.format(self.file_load_model))
+                self.neural_network.model.load_weights('{}.h5'.format('models_saved/model'))
                 self.neural_network.model.compile(loss=self.loss, optimizer=self.optimizer, metrics=self.metrics)
-            logging.debug('Loaded file {}.h5'.format(self.file_load_model))
+                print('\nRede Neural Carregada ------------------------------------------\n')
+            logging.debug('Loaded file {}.h5'.format('models_saved/model'))
 
             logging.debug('Neural network compiled: {} {} {} '.format(self.loss, self.optimizer, self.metrics))
 
@@ -83,49 +85,45 @@ class Neural:
 
             if self.adversarial_model:
 
-                model_architecture_json = self.neural_network.generator_model.to_json()
+                model_architecture_json = self.neural_network.model.to_json()
             else:
 
                 model_architecture_json = self.neural_network.model.to_json()
 
-            with open('{}.json'.format(self.file_save_models), "w") as json_file:
+            with open('{}.json'.format('models_saved/model'), "w") as json_file:
 
-                logging.debug('Write file {}.json'.format(self.file_save_models))
+                logging.debug('Write file {}.json'.format('models_saved/model'))
                 json_file.write(model_architecture_json)
 
-                logging.debug('Write file {}.h5'.format(self.file_save_models))
+                logging.debug('Write file {}.h5'.format('models_saved/model'))
 
                 if self.adversarial_model:
 
-                    self.neural_network.generator_model.save_weights('{}.h5'.format(self.file_save_models))
+                    self.neural_network.model.save_weights('{}.h5'.format('models_saved/model'))
                 else:
 
-                    self.neural_network.model.save_weights('{}.h5'.format(self.file_save_models))
+                    self.neural_network.model.save_weights('{}.h5'.format('models_saved/model'))
 
+                print('Rede neural salva')
         except FileNotFoundError:
 
             logging.error('Path not found: Path {}'.format(self.file_save_models.split('/')[:-1]))
             exit(-1)
 
-    def training(self, training_set, evaluation_set=None):
+    def training(self, training_set_in, training_set_out):
 
         logging.info('Training neural network model')
-        x_sample, y_sample, x_eval, y_eval = self.neural_network.adapter_input(training_set, evaluation_set)
-        x_training_set = numpy.array(x_sample, dtype=numpy.float32)
-        y_training_set = numpy.array(y_sample, dtype=numpy.float32)
-        x_training_set = x_training_set.reshape((int(x_sample.size/int(self.feature_window_width*self.feature_window_length)), self.feature_window_width, self.feature_window_length, 1))
-        y_training_set = y_training_set.reshape((int(y_sample.size/int(self.feature_window_width*self.feature_window_length)), self.feature_window_width, self.feature_window_length, 1))
+        x_sample, y_sample = self.neural_network.adapter_input(training_set_in, training_set_out)
+        x_training_set = x_sample.reshape((int(x_sample.size / int(
+            self.feature_window_width * self.feature_window_length)), self.feature_window_width,
+                                           self.feature_window_length, 1))
 
-        if evaluation_set is not None:
+        y_training_set = y_sample.reshape((int(y_sample.size / int(
+            self.feature_window_width * self.feature_window_length)), self.feature_window_width,
+                                           self.feature_window_length, 1))
 
-            x_evaluation_set = numpy.array(x_eval, dtype=numpy.float32)
-            evaluation_set_data = [x_evaluation_set, y_eval]
-            logging.info('Neural network sample shape x= {} y= {}'.format(x_training_set.shape, y_training_set.shape))
-            self.neural_network.training(x_training_set, y_training_set, evaluation_set_data)
-        else:
-
-            logging.info('Neural network sample shape x= {} y= {}'.format(x_training_set.shape, y_training_set.shape))
-            self.neural_network.training(x_training_set, y_sample, None)
+        logging.info('Neural network sample shape x= {} y= {}'.format(x_training_set.shape, y_training_set.shape))
+        self.neural_network.training(x_training_set, y_training_set, None)
 
         return 0
 
