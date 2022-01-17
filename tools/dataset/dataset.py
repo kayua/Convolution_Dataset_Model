d35164559baf64fc39006328c89a76f23fa737e7
kayua <kayuaolequesp@gmail.com>
2022-01-17 17:38:22 -0300

    update

--- tools/dataset/dataset.py
+++ tools/dataset/dataset.py
@@ -98,20 +98,22 @@ class Dataset:
     def cast_feature_to_swarm(self, feature, position, pointer, predict_input_samples):
 
         results = self.restore_matrix(feature)
+        predict_original_samples = self.restore_matrix(predict_input_samples)
 
         for i in range(len(results)):
 
             for j in range(len(results[0])):
 
-                if (float(results[i][j][0]) > self.threshold) or (float(predict_input_samples[i][j][0]) > self.threshold):
+                if (float(results[i][j][0]) > self.threshold) or (float(predict_original_samples[i][j][0]) > self.threshold):
 
                     pointer.write('{} {}\n'.format(j+position+1, i))
 
     def cast_all_features_to_swarm(self, features_predicted, predict_input_samples):
 
         output = open(self.output_file_swarm, 'w')
+        input_feature = predict_input_samples.reshape((len(predict_input_samples), self.feature_window_width, self.feature_window_length, 1))
         for i, j in enumerate(range(0, len(features_predicted), self.number_block_per_samples)):
-            self.cast_feature_to_swarm(features_predicted[j:j+self.number_block_per_samples], i*self.feature_window_length, output, predict_input_samples)
+            self.cast_feature_to_swarm(features_predicted[j:j+self.number_block_per_samples], i*self.feature_window_length, output, input_feature[j:j+self.number_block_per_samples])
         output.close()
         self.sort(self.output_file_swarm, self.output_file_swarm)
 
