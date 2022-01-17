fadd72951295001a071702514be1551a46a8e8cf
kayua <kayuaolequesp@gmail.com>
2022-01-17 10:28:30 -0300

    update

--- models/neural.py
+++ models/neural.py
@@ -131,6 +131,7 @@ class Neural:
 
     def predict(self, sample_x):
 
+        logging.info('Waiting: Predicting samples')
         return self.neural_network.model.predict(x=sample_x)
 
     def calibration_neural_network(self):
